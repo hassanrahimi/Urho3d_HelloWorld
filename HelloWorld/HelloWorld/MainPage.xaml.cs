331c87ea63a0d5d12d3457cec246b0b38182d29b
hassanrahimi <37825598+hassanrahimi@users.noreply.github.com>
2018-09-18 16:19:38 +0430

    Add project files.

---
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Urho;
+
+namespace HelloWorld
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+           await HelloWorldUrhoSharp.Show<MyHelloWord>(new Urho.ApplicationOptions(assetsFolder: null) { Orientation = ApplicationOptions.OrientationType.LandscapeAndPortrait });
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/MyHelloWord.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Urho;
+using Urho.Gui;
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public class MyHelloWord : Urho.Application
+    {
+       
+        public MyHelloWord(ApplicationOptions options=null) : base(options)
+        {
+            
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+            CreateText();
+        }
+        private void CreateText()
+        {
+            var label = new Label()
+            {
+
+                Text = "سلام",
+                VerticalTextAlignment = TextAlignment.Center,
+                HorizontalTextAlignment =TextAlignment.Center
+            };
+
+            var text = new Text()
+            {
+                //Value = "سلام به همه این اولین برنامه من است با نام دنیازی من",
+                Value="سلام",
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                FontSize=40,
+                
+                
+               
+            };
+
+            text.SetColor(Urho.Color.Cyan);
+            text.SetFont(font: ResourceCache.GetFont("Fonts/Anonymous Pro.ttf"),size: 40);
+            //text.SetFont(font: ResourceCache.GetFont("Fonts/Yekan.ttf"), size: 40);
+
+            //add to UI Root
+
+            //UI.Root.AddChild(text);
+            UI.Root.AddChild(text);
+
+          
+        }
+    }
+}
